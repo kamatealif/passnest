58e38ae1e3c4d4f6ebdd8893afe4aad6559a4f69
kamatealif <kamatealip1@gmail.com>
2025-10-05 20:13:40 +0530

    routes added

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr, Field
+from typing import Annotated
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+class User(BaseModel):
+    email: Annotated[EmailStr, Field(..., description="Email of the user")]
+    password: Annotated[str, Field(..., description="Password of the user")]
+    
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post('/login')
+async def login_user(user_credentials : User):
+    return {"message": "Login successful"}
+
+@app.post('/register')
+async def register_user(user_credentials : User):
+    
